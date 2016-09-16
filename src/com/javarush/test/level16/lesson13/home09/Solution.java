9dd1d02a816ca7daf3c09bcd61be7eceaf755590
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-16 11:44:55 +0300

    Added level16.lesson13.home09

---
+++ src/com/javarush/test/level16/lesson13/home09/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level16.lesson13.home09;
+
+/* Только по-очереди!
+1. В классе Solution создать нить public static Read3Strings унаследовавшись от Thread.
+2. В методе run реализовать чтение с консоли трех строк.
+3. Три подряд введенных строки должны считываться в одной нити и объединяться в одну строку через пробел.
+4. В методе main вывести результат для каждой нити.
+5. Используйте join
+
+Пример:
+Входные данные
+a
+b
+c
+d
+e
+f
+Выходные данные:
+a b c
+d e f
+*/
+
+public class Solution {
+    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws InterruptedException {
+        Read3Strings t1 = new Read3Strings();
+        Read3Strings t2 = new Read3Strings();
+        t1.start();
+        t2.start();
+        System.out.println(/*print t1 result here*/);
+        System.out.println(/*print t2 result here*/);
+    }
+}
