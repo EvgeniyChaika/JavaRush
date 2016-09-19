a451fd61efb59e329aa66a6744d5d13a01a57b8d
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-19 19:34:53 +0300

    Solved level17.lesson02.task02

--- src/com/javarush/test/level17/lesson02/task02/Solution.java
+++ src/com/javarush/test/level17/lesson02/task02/Solution.java
@@ -40,4 +40,12 @@ public class Solution {
             }
         }
     }
+
+    public static class SortThread extends Thread {
+
+        @Override
+        public void run() {
+            sort(testArray);
+        }
+    }
 }
