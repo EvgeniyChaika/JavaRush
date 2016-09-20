afe38f2632e88e96f0ceeb722b6813bc15ca88d9
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-20 16:20:58 +0300

    Added level17.lesson10.home01

---
+++ src/com/javarush/test/level17/lesson10/home01/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level17.lesson10.home01;
+
+import java.util.*;
+
+/* Общий список
+1. Изменить класс Solution так, чтобы он стал списком. (Необходимо реализовать интерфейс java.util.List).
+2. Список Solution должен работать только с целыми числами Long.
+3. Воспользуйтесь полем original.
+4. Список будет использоваться нитями, поэтому позаботьтесь, чтобы все методы были синхронизированы.
+*/
+
+public class Solution {
+    private ArrayList<Long> original = new ArrayList<Long>();
+}
