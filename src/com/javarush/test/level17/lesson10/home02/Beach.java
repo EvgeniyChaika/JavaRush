63feaff44419502e14f7d819615d0422aab8d767
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-20 17:10:41 +0300

    Added level17.lesson10.home02

---
+++ src/com/javarush/test/level17/lesson10/home02/Beach.java
@@ -0,0 +1,41 @@
+package com.javarush.test.level17.lesson10.home02;
+
+/* Comparable
+Реализуйте интерфейс Comparable<Beach> в классе Beach, который будет использоваться нитями.
+*/
+
+public class Beach {
+    private String name;      //название
+    private float distance;   //расстояние
+    private int quality;    //качество
+
+    public Beach(String name, float distance, int quality) {
+        this.name = name;
+        this.distance = distance;
+        this.quality = quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+}
