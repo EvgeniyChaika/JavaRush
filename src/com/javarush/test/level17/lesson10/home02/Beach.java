71586f24a03d954114dca519d2e565d9179c7c24
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-21 10:48:19 +0300

    Solved level17.lesson10.home02

--- src/com/javarush/test/level17/lesson10/home02/Beach.java
+++ src/com/javarush/test/level17/lesson10/home02/Beach.java
@@ -4,7 +4,7 @@ package com.javarush.test.level17.lesson10.home02;
 Реализуйте интерфейс Comparable<Beach> в классе Beach, который будет использоваться нитями.
 */
 
-public class Beach {
+public class Beach implements Comparable<Beach> {
     private String name;      //название
     private float distance;   //расстояние
     private int quality;    //качество
@@ -15,27 +15,34 @@ public class Beach {
         this.quality = quality;
     }
 
-    public String getName() {
+    public synchronized String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public synchronized void setName(String name) {
         this.name = name;
     }
 
-    public float getDistance() {
+    public synchronized float getDistance() {
         return distance;
     }
 
-    public void setDistance(float distance) {
+    public synchronized void setDistance(float distance) {
         this.distance = distance;
     }
 
-    public int getQuality() {
+    public synchronized int getQuality() {
         return quality;
     }
 
-    public void setQuality(int quality) {
+    public synchronized void setQuality(int quality) {
         this.quality = quality;
+    }
+
+    @Override
+    public synchronized int compareTo(Beach o) {
+        int distanceParam = (int) (distance - o.getDistance());
+        int qualityParam = quality - o.getQuality();
+        return 1000 * name.compareTo(o.getName()) + 100 * distanceParam + qualityParam;
     }
 }
