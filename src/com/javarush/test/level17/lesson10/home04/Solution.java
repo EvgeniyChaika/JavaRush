a21d9dadcdef80318cc8d0b211e19127a63de03b
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-21 13:59:11 +0300

    Solved level17.lesson10.home04

--- src/com/javarush/test/level17/lesson10/home04/Solution.java
+++ src/com/javarush/test/level17/lesson10/home04/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
         double i = method3();
     }
 
-    protected void method1(String param1) {
+    protected synchronized void method1(String param1) {
         Solution solution = new Solution();
         solution.method0();
     }
@@ -22,13 +22,13 @@ public class Solution {
         param1++;
     }
 
-    double method3() {
+    synchronized double method3() {
         double random = Math.random();
         param += 40.7;
         return random + param;
     }
 
-    private void method4() {
+    private synchronized void method4() {
         sb.append(1).append(1).append(1).append(1);
     }
 
@@ -36,7 +36,7 @@ public class Solution {
         new StringBuffer().append(param2).append(param2).append(param2);
     }
 
-    public String method6(int param2) {
+    public synchronized String method6(int param2) {
         System.out.println("Thinking....");
         method7(5e-2);
         sb = new StringBuilder("Got it!.");
