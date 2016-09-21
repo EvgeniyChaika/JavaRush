074ee56a4e42a481e44ef23a80a43e7cc9bfdda2
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-21 15:58:22 +0300

    Solved level17.lesson10.home07

--- src/com/javarush/test/level17/lesson10/home07/ApplicationContext.java
+++ src/com/javarush/test/level17/lesson10/home07/ApplicationContext.java
@@ -17,11 +17,11 @@ public abstract class ApplicationContext<GenericsBean extends Bean> {
         parseAllClassesAndInterfaces();
     }
 
-    public GenericsBean getByName(String name) {
+    public synchronized GenericsBean getByName(String name) {
           return container.get(name);
     }
 
-    public GenericsBean removeByName(String name) {
+    public synchronized GenericsBean removeByName(String name) {
         return container.remove(name);
     }
 
