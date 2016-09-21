0c00f7df12a7b5ad4102b9474397fbe07a8fb0ad
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-21 15:48:15 +0300

    Solved level17.lesson10.home05

--- src/com/javarush/test/level17/lesson10/home05/Solution.java
+++ src/com/javarush/test/level17/lesson10/home05/Solution.java
@@ -4,62 +4,54 @@ package com.javarush.test.level17.lesson10.home05;
 synchronized существенно замедляет программу, поэтому убери избыточность synchronized внутри методов
 */
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectInputStream.GetField;
+import java.io.ObjectOutputStream;
+import java.io.ObjectOutputStream.PutField;
+import java.io.ObjectStreamField;
+
 public class Solution {
     char[] value;
     int count;
 
     public Solution append(CharSequence s) {
-        synchronized (Solution.class) {
-            if (s == null) {
-                synchronized (this) {
-                    s = "null";
-                }
-            }
+        if (s == null) {
+            s = "null";
+        }
 
-            if (s instanceof String) {
-                synchronized (this) {
-                    return this.append((String) s);
-                }
-            }
+        if (s instanceof String) {
+            return this.append((String) s);
+        }
 
-            if (s instanceof Solution) {
-                synchronized (this) {
-                    return this.appendThis((Solution) s);
-                }
-            }
+        if (s instanceof Solution) {
+            return this.appendThis((Solution) s);
         }
         return this.append(s);
     }
 
     public synchronized Solution appendThis(Solution s) {
-        //do something here....
         return this;
     }
 
-    private static final java.io.ObjectStreamField[] serialPersistentFields =
+    private static final ObjectStreamField[] serialPersistentFields =
             {
-                    new java.io.ObjectStreamField("value", char[].class),
-                    new java.io.ObjectStreamField("count", Integer.TYPE),
-                    new java.io.ObjectStreamField("shared", Boolean.TYPE),
+                    new ObjectStreamField("value", char[].class),
+                    new ObjectStreamField("count", Integer.TYPE),
+                    new ObjectStreamField("shared", Boolean.TYPE),
             };
 
-    private synchronized void writeObject(java.io.ObjectOutputStream s) throws java.io.IOException {
-        java.io.ObjectOutputStream.PutField fields = s.putFields();
-        synchronized (fields) {
-            fields.put("value", value);
-            fields.put("count", count);
-            fields.put("shared", false);
-        }
-        synchronized (s) {
-            s.writeFields();
-        }
+    private synchronized void writeObject(ObjectOutputStream s) throws IOException {
+        PutField fields = s.putFields();
+        fields.put("value", value);
+        fields.put("count", count);
+        fields.put("shared", false);
+        s.writeFields();
     }
 
-    private void readObject(java.io.ObjectInputStream s) throws java.io.IOException, ClassNotFoundException {
-        synchronized (new java.io.IOException()) {
-            java.io.ObjectInputStream.GetField fields = s.readFields();
-            value = (char[]) fields.get("value", null);
-            count = fields.get("count", 0);
-        }
+    private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+        GetField fields = s.readFields();
+        value = (char[]) fields.get("value", null);
+        count = fields.get("count", 0);
     }
 }
