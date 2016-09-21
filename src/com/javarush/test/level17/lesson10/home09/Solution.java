0d00d265b6f24f01f5b9181577cb50c9ae105bf6
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-21 16:10:24 +0300

    Added level17.lesson10.home09

---
+++ src/com/javarush/test/level17/lesson10/home09/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level17.lesson10.home09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* Транзакционность
+Сделать метод joinData транзакционным, т.е. если произошел сбой, то данные не должны быть изменены.
+1. Считать с консоли 2 имени файла
+2. Считать построчно данные из файлов. Из первого файла - в allLines, из второго - в forRemoveLines
+В методе joinData:
+3. Если список allLines содержит все строки из forRemoveLines, то удалить из списка allLines все строки, которые есть в forRemoveLines
+4. Если список allLines НЕ содержит каких-либо строк, которые есть в forRemoveLines, то
+4.1. очистить allLines от данных
+4.2. выбросить исключение CorruptedDataException
+Метод joinData должен вызываться в main. Все исключения обработайте в методе main.
+*/
+
+public class Solution {
+    public static List<String> allLines = new ArrayList<String>();
+    public static List<String> forRemoveLines = new ArrayList<String>();
+
+    public static void main(String[] args) {
+    }
+
+    public void joinData () throws CorruptedDataException {
+
+    }
+}
