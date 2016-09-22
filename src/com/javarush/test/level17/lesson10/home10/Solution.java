d3a2d5881225c78e02f39e45341f70161c7af1aa
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-22 17:54:09 +0300

    Solved level17.lesson10.home10

--- src/com/javarush/test/level17/lesson10/home10/Solution.java
+++ src/com/javarush/test/level17/lesson10/home10/Solution.java
@@ -17,6 +17,11 @@ public class Solution {
         counter3.start();
         counter4.start();
 
+        counter1.join();
+        counter2.join();
+        counter3.join();
+        counter4.join();
+
         for (int i = 1; i <= 100; i++) {
             if (values[i] != 1) {
                 System.out.println("Массив values содержит элементы неравные 1");
@@ -46,7 +51,7 @@ public class Solution {
         @Override
         public void run() {
             do {
-                synchronized (this) {
+                synchronized (Solution.class) {
                     incrementCount();
                     values[getCount()]++;
                 }
