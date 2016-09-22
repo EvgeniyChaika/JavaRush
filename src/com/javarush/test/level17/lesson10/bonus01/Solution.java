d4bf57c84adcffc3733a707bb3782b0f18a031c7
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-22 17:59:37 +0300

    Added level17.lesson10.bonus01

---
+++ src/com/javarush/test/level17/lesson10/bonus01/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.test.level17.lesson10.bonus01;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/* CRUD
+CrUD - Create, Update, Delete
+Программа запускается с одним из следующих наборов параметров:
+-c name sex bd
+-u id name sex bd
+-d id
+-i id
+Значения параметров:
+name - имя, String
+sex - пол, "м" или "ж", одна буква
+bd - дата рождения в следующем формате 15/04/1990
+-c  - добавляет человека с заданными параметрами в конец allPeople, выводит id (index) на экран
+-u  - обновляет данные человека с данным id
+-d  - производит логическое удаление человека с id
+-i  - выводит на экран информацию о человеке с id: name sex (м/ж) bd (формат 15-Apr-1990)
+
+id соответствует индексу в списке
+Все люди должны храниться в allPeople
+Используйте Locale.ENGLISH в качестве второго параметра для SimpleDateFormat
+
+Пример параметров: -c Миронов м 15/04/1990
+*/
+
+public class Solution {
+    public static List<Person> allPeople = new ArrayList<Person>();
+    static {
+        allPeople.add(Person.createMale("Иванов Иван", new Date()));  //сегодня родился    id=0
+        allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
+    }
+
+    public static void main(String[] args) {
+        //start here - начни тут
+    }
+}
