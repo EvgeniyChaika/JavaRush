fa4420a631e3cb4e3f819da31b93dec802d66714
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-23 13:01:07 +0300

    Solved level18.lesson03.task02

--- src/com/javarush/test/level18/lesson03/task02/Solution.java
+++ src/com/javarush/test/level18/lesson03/task02/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.test.level18.lesson03.task02;
 
-/* Минимальный байт
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;/* Минимальный байт
 Ввести с консоли имя файла
 Найти минимальный байт в файле, вывести его на экран.
 Закрыть поток ввода-вывода
@@ -8,5 +10,20 @@ package com.javarush.test.level18.lesson03.task02;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String data = reader.readLine();
+
+        FileInputStream inputStream = new FileInputStream(data);
+
+        int small = Integer.MAX_VALUE;
+        while (inputStream.available() > 0) {
+            int bit = inputStream.read();
+            if (bit < small) {
+                small = bit;
+            }
+        }
+        inputStream.close();
+        System.out.println(small);
     }
 }
