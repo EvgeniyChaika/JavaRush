8217486e09a189879bc1955cee0c0736c30557fc
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-23 15:33:54 +0300

    Added level18.lesson03.task05

---
+++ src/com/javarush/test/level18/lesson03/task05/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level18.lesson03.task05;
+
+import java.io.FileInputStream;
+
+
+
+
+/* Сортировка байт
+Ввести с консоли имя файла
+Считать все байты из файла.
+Не учитывая повторений - отсортировать их по байт-коду в возрастающем порядке.
+Вывести на экран
+Закрыть поток ввода-вывода
+
+Пример байт входного файла
+44 83 44
+
+Пример вывода
+44 83
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
