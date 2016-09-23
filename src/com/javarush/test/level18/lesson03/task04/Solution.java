f2cc857f0ba6ea63f64020e13e9ed8aad1ebf685
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-23 15:27:40 +0300

    Solved level18.lesson03.task04

--- src/com/javarush/test/level18/lesson03/task04/Solution.java
+++ src/com/javarush/test/level18/lesson03/task04/Solution.java
@@ -7,7 +7,49 @@ package com.javarush.test.level18.lesson03.task04;
 Закрыть поток ввода-вывода
 */
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream inp = new FileInputStream(r.readLine());
+        String res = "";
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> reslist = new ArrayList<Integer>();
+
+        while (inp.available() > 0) {
+            int data = inp.read();
+            list.add(data);
+        }
+
+        int[] count = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            count[i] = 0;
+            for (int j = 0; j < list.size(); j++) {
+                if (list.get(i).equals(list.get(j))) count[i]++;
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int h : count) {
+            if (h < min) {
+                min = h;
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (!reslist.contains(list.get(i)) && count[i] == min) {
+                reslist.add(list.get(i));
+            }
+        }
+        for (int y : reslist) {
+            res += y + " ";
+        }
+        System.out.println(res);
+        r.close();
+        inp.close();
     }
 }
