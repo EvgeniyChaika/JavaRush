78b4b77e259f0faf5bfb302103b634c4d38482d2
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-23 13:01:56 +0300

    Added level18.lesson03.task03

---
+++ src/com/javarush/test/level18/lesson03/task03/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level18.lesson03.task03;
+
+import java.io.FileInputStream;
+
+/* Самые частые байты
+Ввести с консоли имя файла
+Найти байт или байты с максимальным количеством повторов
+Вывести их на экран через пробел
+Закрыть поток ввода-вывода
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
