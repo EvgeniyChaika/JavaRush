0c0a9357deb88e0408576ee3fc82a17ca8d32941
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-23 12:55:01 +0300

    Solved level18.lesson03.task01

--- src/com/javarush/test/level18/lesson03/task01/Solution.java
+++ src/com/javarush/test/level18/lesson03/task01/Solution.java
@@ -1,14 +1,25 @@
 package com.javarush.test.level18.lesson03.task01;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-
-/* Максимальный байт
-Ввести с консоли имя файла
-Найти максимальный байт в файле, вывести его на экран.
-Закрыть поток ввода-вывода
-*/
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String data = reader.readLine();
+
+        FileInputStream inputStream = new FileInputStream(data);
+
+        int big = 0;
+        while (inputStream.available() > 0) {
+            int bit = inputStream.read();
+            if (bit > big) {
+                big = bit;
+            }
+        }
+        inputStream.close();
+        System.out.println(big);
     }
 }
