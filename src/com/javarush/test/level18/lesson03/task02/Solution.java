f9ad3fd2de76000db592e0cd11b8335870c0f2ea
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-23 12:56:35 +0300

    Added level18.lesson03.task02

---
+++ src/com/javarush/test/level18/lesson03/task02/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level18.lesson03.task02;
+
+/* Минимальный байт
+Ввести с консоли имя файла
+Найти минимальный байт в файле, вывести его на экран.
+Закрыть поток ввода-вывода
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
