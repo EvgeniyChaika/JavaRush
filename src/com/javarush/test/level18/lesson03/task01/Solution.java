e1c5655816a3e3ce7c81d2d7dd00941356b87249
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-23 12:47:06 +0300

    Added level18.lesson03.task01

---
+++ src/com/javarush/test/level18/lesson03/task01/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level18.lesson03.task01;
+
+import java.io.FileInputStream;
+
+/* Максимальный байт
+Ввести с консоли имя файла
+Найти максимальный байт в файле, вывести его на экран.
+Закрыть поток ввода-вывода
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
