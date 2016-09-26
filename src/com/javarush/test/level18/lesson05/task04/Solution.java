30b5c76104b25b2018d356f23f2523436e0e3375
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-26 17:35:17 +0300

    Added level18.lesson05.task04

---
+++ src/com/javarush/test/level18/lesson05/task04/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level18.lesson05.task04;
+
+/* Реверс файла
+Считать с консоли 2 имени файла: файл1, файл2.
+Записать в файл2 все байты из файл1, но в обратном порядке
+Закрыть потоки. Не использовать try-with-resources
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
