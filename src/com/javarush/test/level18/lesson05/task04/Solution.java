beb823ca2e0939fc82df66b47ea7ae016bf55cbb
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-26 18:04:27 +0300

    Solved level18.lesson05.task04

--- src/com/javarush/test/level18/lesson05/task04/Solution.java
+++ src/com/javarush/test/level18/lesson05/task04/Solution.java
@@ -6,8 +6,34 @@ package com.javarush.test.level18.lesson05.task04;
 Закрыть потоки. Не использовать try-with-resources
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String file1 = reader.readLine();
+        String file2 = reader.readLine();
+
+        FileInputStream inputStream = new FileInputStream(file1);
+        FileOutputStream outputStream = new FileOutputStream(file2);
+
+        while (inputStream.available() > 0) {
+            byte[] buffer = new byte[inputStream.available()];
+            int count = inputStream.read(buffer);
+
+            for (int i = 0; i < buffer.length / 2; i++) {
+                byte temp = buffer[i];
+                buffer[i] = buffer[buffer.length - 1 - i];
+                buffer[buffer.length - 1 - i] = temp;
+            }
+
+            outputStream.write(buffer, 0, count);
+        }
+
+        reader.close();
+        inputStream.close();
+        outputStream.close();
     }
 }
