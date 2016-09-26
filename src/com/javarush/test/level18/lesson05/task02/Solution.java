1dcb1a8ff44458085387ce68accff1ad275848b7
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-26 16:14:10 +0300

    Added level18.lesson05.task02

---
+++ src/com/javarush/test/level18/lesson05/task02/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level18.lesson05.task02;
+
+/* Подсчет запятых
+С консоли считать имя файла
+Посчитать в файле количество символов ',', количество вывести на консоль
+Закрыть потоки. Не использовать try-with-resources
+
+Подсказка: нужно сравнивать с ascii-кодом символа ','
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
