f6ec0939e5b9d6f4bf06614aadbd587fbedf0a47
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-26 17:34:30 +0300

    Solved level18.lesson05.task03

--- src/com/javarush/test/level18/lesson05/task03/Solution.java
+++ src/com/javarush/test/level18/lesson05/task03/Solution.java
@@ -8,8 +8,36 @@ package com.javarush.test.level18.lesson05.task03;
 Закрыть потоки. Не использовать try-with-resources
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String file1 = reader.readLine();
+        String file2 = reader.readLine();
+        String file3 = reader.readLine();
+
+        FileInputStream inputStream = new FileInputStream(file1);
+        FileOutputStream outputStream1 = new FileOutputStream(file2);
+        FileOutputStream outputStream2 = new FileOutputStream(file3);
+
+        while (inputStream.available() > 0) {
+            byte[] buffer = new byte[inputStream.available()];
+            int count = inputStream.read(buffer);
+            if (count % 2 == 1) {
+                int med = (count + 1) / 2;
+                outputStream1.write(buffer, 0, med);
+                outputStream2.write(buffer, med , count - med);
+            } else {
+                outputStream1.write(buffer, 0, count / 2);
+                outputStream2.write(buffer, count / 2, count - count / 2);
+            }
+        }
+        reader.close();
+        inputStream.close();
+        outputStream1.close();
+        outputStream2.close();
     }
 }
