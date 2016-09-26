6ec77fba59db5025e8b03459a2794135437c4331
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-26 16:29:10 +0300

    Solved level18.lesson05.task02

--- src/com/javarush/test/level18/lesson05/task02/Solution.java
+++ src/com/javarush/test/level18/lesson05/task02/Solution.java
@@ -8,7 +8,29 @@ package com.javarush.test.level18.lesson05.task02;
 Подсказка: нужно сравнивать с ascii-кодом символа ','
 */
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String data = reader.readLine();
+
+        FileInputStream inputStream = new FileInputStream(data);
+
+        int sum = 0;
+        while (inputStream.available() > 0) {
+            int bite = inputStream.read();
+            if (bite == 44) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+        reader.close();
+        inputStream.close();
     }
 }
