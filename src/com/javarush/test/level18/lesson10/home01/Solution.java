e88a6cc80023110b6657162aea4b9ba401a60a06
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-27 15:33:03 +0300

    Solved level18.lesson10.home01

--- src/com/javarush/test/level18/lesson10/home01/Solution.java
+++ src/com/javarush/test/level18/lesson10/home01/Solution.java
@@ -7,7 +7,23 @@ package com.javarush.test.level18.lesson10.home01;
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 0) {
+            FileInputStream fileInputStream = new FileInputStream(args[0]);
+
+            int sum = 0;
+            while (fileInputStream.available() > 0) {
+                int bite = fileInputStream.read();
+                if ((bite >= 65 && bite <= 90) || (bite >= 97 && bite <= 122)) {
+                    sum++;
+                }
+            }
+            System.out.println(sum);
+            fileInputStream.close();
+        }
     }
 }
