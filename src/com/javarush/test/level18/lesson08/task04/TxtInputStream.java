78aad0382631fa6689f979642c657baed3ee4aae
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-27 14:27:19 +0300

    Solved level18.lesson08.task04

--- src/com/javarush/test/level18/lesson08/task04/TxtInputStream.java
+++ src/com/javarush/test/level18/lesson08/task04/TxtInputStream.java
@@ -3,6 +3,7 @@ package com.javarush.test.level18.lesson08.task04;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 /* UnsupportedFileName
 Измените класс TxtInputStream так, чтобы он работал только с txt-файлами (*.txt)
@@ -10,9 +11,68 @@ import java.io.IOException;
 Если передан не txt-файл, например, file.txt.exe, то конструктор должен выбрасывать исключение UnsupportedFileNameException
 */
 
-public class TxtInputStream extends FileInputStream {
-    public TxtInputStream(String fileName) {
+public class TxtInputStream extends InputStream {
+
+    private FileInputStream fileInputStream;
+
+    public TxtInputStream(String fileName) throws FileNotFoundException, UnsupportedFileNameException {
+        char[] arr = fileName.toCharArray();
+        String str = new String(arr, arr.length - 4, 4);
+        if ((".txt").equals(str)) {
+            this.fileInputStream = new FileInputStream(fileName);
+        } else {
+            throw new UnsupportedFileNameException();
+        }
+
     }
 
+    public TxtInputStream() {
+        super();
+    }
+
+    @Override
+    public int read() throws IOException {
+        return fileInputStream.read();
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        return fileInputStream.read(b);
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        return fileInputStream.read(b, off, len);
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        return fileInputStream.skip(n);
+    }
+
+    @Override
+    public int available() throws IOException {
+        return fileInputStream.available();
+    }
+
+    @Override
+    public void close() throws IOException {
+        fileInputStream.close();
+    }
+
+    @Override
+    public synchronized void mark(int readlimit) {
+        fileInputStream.mark(readlimit);
+    }
+
+    @Override
+    public synchronized void reset() throws IOException {
+        fileInputStream.reset();
+    }
+
+    @Override
+    public boolean markSupported() {
+        return fileInputStream.markSupported();
+    }
 }
 
