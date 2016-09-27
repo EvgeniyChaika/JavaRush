0735729a3ce6cfd2d31c764c9656cef176c4342e
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-27 14:39:43 +0300

    Solved level18.lesson08.task05

--- src/com/javarush/test/level18/lesson08/task05/Solution.java
+++ src/com/javarush/test/level18/lesson08/task05/Solution.java
@@ -9,8 +9,29 @@ import java.util.List;
 */
 
 public class Solution {
-    public class TableInterfaceWrapper {
+    public class TableInterfaceWrapper implements ATableInterface {
 
+        private ATableInterface aTableInterface;
+
+        public TableInterfaceWrapper(ATableInterface aTableInterface) {
+            this.aTableInterface = aTableInterface;
+        }
+
+        @Override
+        public void setModel(List rows) {
+            System.out.println(rows.size());
+            aTableInterface.setModel(rows);
+        }
+
+        @Override
+        public String getHeaderText() {
+            return aTableInterface.getHeaderText().toUpperCase();
+        }
+
+        @Override
+        public void setHeaderText(String newHeaderText) {
+            aTableInterface.setHeaderText(newHeaderText);
+        }
     }
 
     public interface ATableInterface {
