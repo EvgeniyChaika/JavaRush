0996d580cb0e69a4cab6fdbe76a5217a0c2b9287
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-27 12:31:40 +0300

    Solved level18.lesson08.task02

--- src/com/javarush/test/level18/lesson08/task02/QuestionFileOutputStream.java
+++ src/com/javarush/test/level18/lesson08/task02/QuestionFileOutputStream.java
@@ -1,6 +1,8 @@
 package com.javarush.test.level18.lesson08.task02;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /* Расширяем AmigoOutputStream
 Используя шаблон проектирования Wrapper (Decorator) расширьте функциональность AmigoOutputStream
@@ -13,5 +15,43 @@ import java.io.*;
 
 public class QuestionFileOutputStream implements AmigoOutputStream {
 
-}
+    private AmigoOutputStream original;
 
+    public QuestionFileOutputStream(AmigoOutputStream original) {
+        this.original = original;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        original.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        original.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        original.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        original.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String string = reader.readLine();
+
+        reader.close();
+        if (("Д").equals(string)) {
+            this.original.close();
+        }
+    }
+}
