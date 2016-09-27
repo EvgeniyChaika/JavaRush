cd7ffb0a43a068ac7026d7d48a7f69ed5ff7d637
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-27 11:57:48 +0300

    Added level18.lesson08.task02

---
+++ src/com/javarush/test/level18/lesson08/task02/QuestionFileOutputStream.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level18.lesson08.task02;
+
+import java.io.*;
+
+/* Расширяем AmigoOutputStream
+Используя шаблон проектирования Wrapper (Decorator) расширьте функциональность AmigoOutputStream
+В классе QuestionFileOutputStream при вызове метода close() должна быть реализована следующая функциональность:
+1. Вывести в консоль фразу [Вы действительно хотите закрыть поток? Д/Н]
+2. Считайте строку
+3. Если считанная строка равна [Д], то закрыть поток
+4. Если считанная строка не равна [Д], то не закрывать поток
+*/
+
+public class QuestionFileOutputStream implements AmigoOutputStream {
+
+}
+
