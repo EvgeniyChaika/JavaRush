aca977bbc800fd23cdf93fc6861d6ce1fc9d39fa
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-27 13:12:01 +0300

    Solved level18.lesson08.task03

--- src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
+++ src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
@@ -1,8 +1,6 @@
 package com.javarush.test.level18.lesson08.task03;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 /* AmigoOutputStream
 1 Измените класс AmigoOutputStream так, чтобы он стал Wrapper-ом для класса FileOutputStream. Используйте наследование.
@@ -12,12 +10,49 @@ import java.io.IOException;
 2.3 закрыть поток методом close()
 */
 
-public class AmigoOutputStream {
-    public static String fileName = "C:/tmp/result.txt";
+public class AmigoOutputStream extends OutputStream {
+    public static String fileName = "/home/evgeniy/Documents/file";
+
+    private FileOutputStream fileOutputStream;
+
+    private String str = "JavaRush © 2012-2013 All rights reserved.";
+
+    public AmigoOutputStream(FileOutputStream fileOutputStream) {
+        this.fileOutputStream = fileOutputStream;
+    }
+
+    public AmigoOutputStream() {
+        super();
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        fileOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        fileOutputStream.write(b);
+    }
+
+    @Override
+    public void close() throws IOException {
+        fileOutputStream.flush();
+        fileOutputStream.write(str.getBytes());
+        fileOutputStream.close();
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         new AmigoOutputStream(new FileOutputStream(fileName));
     }
-
-}
 }
