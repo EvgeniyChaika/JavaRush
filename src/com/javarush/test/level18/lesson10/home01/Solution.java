d35b9c553889a920e2a98578a67753e11725ec75
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-27 14:53:16 +0300

    Added level18.lesson10.home01

---
+++ src/com/javarush/test/level18/lesson10/home01/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level18.lesson10.home01;
+
+/* Английские буквы
+В метод main первым параметром приходит имя файла.
+Посчитать количество букв английского алфавита, которое есть в этом файле.
+Вывести на экран число (количество букв)
+Закрыть потоки. Не использовать try-with-resources
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
