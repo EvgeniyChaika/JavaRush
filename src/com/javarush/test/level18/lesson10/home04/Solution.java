f8e8e017141ca7a18491851aecf553c35b44f31c
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-28 12:53:52 +0300

    Added level18.lesson10.home04

---
+++ src/com/javarush/test/level18/lesson10/home04/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level18.lesson10.home04;
+
+/* Объединение файлов
+Считать с консоли 2 имени файла
+В начало первого файла записать содержимое второго файла так, чтобы получилось объединение файлов
+Закрыть потоки. Не использовать try-with-resources
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
