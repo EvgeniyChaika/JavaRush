58a93886f16c7199ec16f69a0fd5c7386e60feb0
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-28 15:27:39 +0300

    Solved level18.lesson10.home04

--- src/com/javarush/test/level18/lesson10/home04/Solution.java
+++ src/com/javarush/test/level18/lesson10/home04/Solution.java
@@ -6,8 +6,45 @@ package com.javarush.test.level18.lesson10.home04;
 Закрыть потоки. Не использовать try-with-resources
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String file1 = reader.readLine();
+        String file2 = reader.readLine();
+
+        FileInputStream inputStream1 = new FileInputStream(file1);
+
+        byte[] buffer1 = new byte[0];
+
+        while (inputStream1.available() > 0) {
+            buffer1 = new byte[inputStream1.available()];
+            int count1 = inputStream1.read(buffer1);
+        }
+
+        FileInputStream inputStream2 = new FileInputStream(file2);
+
+        byte[] buffer2 = new byte[0];
+        while (inputStream2.available() > 0) {
+            buffer2 = new byte[inputStream2.available()];
+            int count2 = inputStream2.read(buffer2);
+        }
+
+        byte[] result = new byte[buffer1.length + buffer2.length];
+
+        System.arraycopy(buffer2, 0, result, 0, buffer2.length);
+        System.arraycopy(buffer1, 0, result, buffer2.length, buffer1.length);
+
+        FileOutputStream outputStream1 = new FileOutputStream(file1);
+
+        outputStream1.write(result);
+
+        reader.close();
+        inputStream1.close();
+        inputStream2.close();
+        outputStream1.close();
     }
 }
