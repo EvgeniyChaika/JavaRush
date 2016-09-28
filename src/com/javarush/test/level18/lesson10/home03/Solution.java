fa5614f42b1c8aa1434b3d39c107e8246981885a
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-28 10:27:54 +0300

    Added level18.lesson10.home03

---
+++ src/com/javarush/test/level18/lesson10/home03/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level18.lesson10.home03;
+
+/* Два в одном
+Считать с консоли 3 имени файла
+Записать в первый файл содержимого второго файла, а потом дописать в первый файл содержимое третьего файла
+Закрыть потоки. Не использовать try-with-resources
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
