c6592e0e49d3b39fd356745a901c01430de76abb
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-28 16:56:23 +0300

    Solved level18.lesson10.home05

--- src/com/javarush/test/level18/lesson10/home05/Solution.java
+++ src/com/javarush/test/level18/lesson10/home05/Solution.java
@@ -14,8 +14,54 @@ package com.javarush.test.level18.lesson10.home05;
 -3.51 - -4
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String file1 = reader.readLine();
+        String file2 = reader.readLine();
+
+        FileInputStream inputStream1 = new FileInputStream(file1);
+
+        byte[] buffer1 = new byte[0];
+
+        while (inputStream1.available() > 0) {
+            buffer1 = new byte[inputStream1.available()];
+            int count1 = inputStream1.read(buffer1);
+        }
+
+        FileOutputStream outputStream1 = new FileOutputStream(file2);
+
+        outputStream1.write(getData(buffer1));
+
+        reader.close();
+        inputStream1.close();
+        outputStream1.close();
+    }
+
+    private static byte[] getData(byte[] data) {
+
+        String[] arr = new String(data).split(" ");
+
+        StringBuilder builder = new StringBuilder();
+
+        byte[] res;
+        long[] tmp = new long[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            tmp[i] = Math.round(Double.valueOf(arr[i]));
+        }
+
+        for (long i : tmp) {
+            builder.append(i);
+            builder.append(" ");
+        }
+
+        res = builder.toString().getBytes();
+
+        return res;
     }
 }
