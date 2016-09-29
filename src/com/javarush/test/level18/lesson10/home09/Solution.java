fbee8555425b2bd90eddd04a053ade395ac33930
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-29 16:24:56 +0300

    Added level18.lesson10.home09

---
+++ src/com/javarush/test/level18/lesson10/home09/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level18.lesson10.home09;
+
+/* Файлы и исключения
+Читайте с консоли имена файлов
+Если файла не существует (передано неправильное имя файла), то
+перехватить исключение FileNotFoundException, вывести в консоль переданное неправильное имя файла и завершить работу программы.
+Закрыть потоки. Не использовать try-with-resources
+Не используйте System.exit();
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
