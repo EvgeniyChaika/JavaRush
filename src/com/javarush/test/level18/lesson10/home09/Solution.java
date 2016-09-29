9f574ecb15eacf68fef05edf7e67b93d436fafc4
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-29 16:48:39 +0300

    Solved level18.lesson10.home09

--- src/com/javarush/test/level18/lesson10/home09/Solution.java
+++ src/com/javarush/test/level18/lesson10/home09/Solution.java
@@ -8,7 +8,23 @@ package com.javarush.test.level18.lesson10.home09;
 Не используйте System.exit();
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream in;
+        String fileName;
+        while (true) {
+            fileName = reader.readLine();
+            try {
+                in = new FileInputStream(fileName);
+                in.close();
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName);
+                reader.close();
+                break;
+            }
+        }
     }
 }
