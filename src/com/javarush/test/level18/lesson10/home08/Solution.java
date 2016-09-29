59216414fc190b4a931ddb794716eef6557534d9
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-29 12:22:51 +0300

    Added level18.lesson10.home08

---
+++ src/com/javarush/test/level18/lesson10/home08/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level18.lesson10.home08;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* Нити и байты
+Читайте с консоли имена файлов, пока не будет введено слово "exit"
+Передайте имя файла в нить ReadThread
+Нить ReadThread должна найти байт, который встречается в файле максимальное число раз, и добавить его в словарь resultMap,
+где параметр String - это имя файла, параметр Integer - это искомый байт.
+Закрыть потоки. Не использовать try-with-resources
+*/
+
+public class Solution {
+    public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class ReadThread extends Thread {
+        public ReadThread(String fileName) {
+            //implement constructor body
+        }
+        // implement file reading here - реализуйте чтение из файла тут
+    }
+}
