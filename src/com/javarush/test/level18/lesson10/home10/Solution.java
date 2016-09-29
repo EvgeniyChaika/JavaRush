fc0c32a8eabe32a53bdc69b95f1ded22964ef260
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-29 18:56:09 +0300

    Solved level18.lesson10.home10

--- src/com/javarush/test/level18/lesson10/home10/Solution.java
+++ src/com/javarush/test/level18/lesson10/home10/Solution.java
@@ -11,7 +11,45 @@ package com.javarush.test.level18.lesson10.home10;
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.*;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Map<Integer, String> mapFiles = new TreeMap<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+        String[] arrStr = new String[0];
+        String fileResult;
+
+        while (!("end").equals(str = reader.readLine())) {
+            arrStr = str.split("\\.");
+            String[] endFile = arrStr[arrStr.length - 1].split("part");
+            mapFiles.put(Integer.valueOf(endFile[1]), str);
+        }
+
+        reader.close();
+
+        StringBuilder builder = new StringBuilder();
+
+        fileResult = builder.append(arrStr[0]).append(".").append(arrStr[1]).toString();
+
+        FileOutputStream outputStream = new FileOutputStream(fileResult);
+
+        for (Map.Entry<Integer, String> entry : mapFiles.entrySet()) {
+            FileInputStream inputStream = new FileInputStream(entry.getValue());
+
+            while (inputStream.available() > 0) {
+                byte[] buffer = new byte[inputStream.available()];
+                int count = inputStream.read(buffer);
+                outputStream.write(buffer);
+            }
+
+            inputStream.close();
+        }
+
+        outputStream.close();
     }
 }
