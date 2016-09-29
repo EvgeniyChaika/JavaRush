6c22de0fde68320e475a875b7b574103ab754025
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-29 11:44:45 +0300

    Solved level18.lesson10.home06

--- src/com/javarush/test/level18/lesson10/home06/Solution.java
+++ src/com/javarush/test/level18/lesson10/home06/Solution.java
@@ -15,8 +15,45 @@ package com.javarush.test.level18.lesson10.home06;
 f 361
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        String file = args[0];
+
+        FileInputStream inputStream = new FileInputStream(file);
+
+        Map<Character, Integer> map = new TreeMap<>();
+
+        char[] arrChars = new char[0];
+
+        while (inputStream.available() > 0) {
+            byte[] buffer = new byte[inputStream.available()];
+            int bite = inputStream.read(buffer);
+            arrChars = new String(buffer).toCharArray();
+        }
+
+        for (char key : arrChars) {
+            if (map.containsKey(key)) {
+                int value = map.get(key) + 1;
+                map.put(key, value);
+            } else {
+                map.put(key, 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+        inputStream.close();
     }
 }
