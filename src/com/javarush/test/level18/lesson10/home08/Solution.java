65228d8ffd093279e9f2c15967e286b66da05344
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-09-29 16:21:48 +0300

    Solved level18.lesson10.home08

--- src/com/javarush/test/level18/lesson10/home08/Solution.java
+++ src/com/javarush/test/level18/lesson10/home08/Solution.java
@@ -1,5 +1,10 @@
 package com.javarush.test.level18.lesson10.home08;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,13 +20,61 @@ public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        ArrayList<String> names = new ArrayList<>();
+        try {
+            String fileName;
+            while (!("exit").equals(fileName = reader.readLine())) {
+                names.add(fileName);
+            }
+
+            reader.close();
+
+            for (String s : names) {
+                Thread thread = new ReadThread(s);
+                thread.start();
+                thread.join();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static class ReadThread extends Thread {
+        private String fileName;
+
         public ReadThread(String fileName) {
-            //implement constructor body
+            this.fileName = fileName;
         }
-        // implement file reading here - реализуйте чтение из файла тут
+
+        @Override
+        public void run() {
+            try {
+                FileInputStream fileReader = new FileInputStream(fileName);
+                ArrayList<Integer> list = new ArrayList<>();
+                while (fileReader.available() > 0) {
+                    int data = fileReader.read();
+                    list.add(data);
+                }
+                fileReader.close();
+
+                int max = 0;
+                int id = 0;
+
+                for (int a = 0; a < list.size(); a++) {
+                    int count = Collections.frequency(list, list.get(a));
+                    if (count > max) {
+                        max = count;
+                        id = list.get(a);
+                    }
+                }
+
+                resultMap.put(fileName, id);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
