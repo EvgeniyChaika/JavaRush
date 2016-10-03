b7f512a4d163c493b927bc27042ae14bc36a2dd0
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-03 11:53:20 +0300

    Added level19.lesson03.task05

---
+++ src/com/javarush/test/level19/lesson03/task05/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.test.level19.lesson03.task05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* Закрепляем адаптер
+Адаптировать Customer и Contact к RowItem.
+Классом-адаптером является DataAdapter.
+Инициализируйте countries перед началом выполнения программы. Соответствие кода страны и названия:
+UA Ukraine
+RU Russia
+CA Canada
+*/
+
+public class Solution {
+    private static Map<String, String> countries = new HashMap<String, String>();
+
+    public static class DataAdapter {
+        public DataAdapter(Customer customer, Contact contact) {
+        }
+    }
+
+    public static interface RowItem {
+        String getCountryCode();        //example UA
+
+        String getCompany();            //example JavaRush Ltd.
+
+        String getContactFirstName();   //example Ivan
+
+        String getContactLastName();    //example Ivanov
+
+        String getDialString();         //example callto://+380501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //example JavaRush Ltd.
+
+        String getCountryName();        //example Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //example Ivanov, Ivan
+
+        String getPhoneNumber();        //example +38(050)123-45-67
+    }
+}
