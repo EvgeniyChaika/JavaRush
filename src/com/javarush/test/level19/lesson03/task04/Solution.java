94718473d272d197c15004a42cdf47339b8243ac
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-03 11:17:09 +0300

    Solved level19.lesson03.task04

--- src/com/javarush/test/level19/lesson03/task04/Solution.java
+++ src/com/javarush/test/level19/lesson03/task04/Solution.java
@@ -1,8 +1,5 @@
 package com.javarush.test.level19.lesson03.task04;
 
-import java.io.IOException;
-import java.util.Scanner;
-
 /* И еще один адаптер
 Адаптировать Scanner к PersonScanner.
 Классом-адаптером является PersonScannerAdapter.
@@ -12,8 +9,34 @@ import java.util.Scanner;
 В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен читать данные одного человека.
 */
 
-public class Solution {
-    public static class PersonScannerAdapter {
+import java.io.IOException;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
+public class Solution {
+
+    public static class PersonScannerAdapter implements PersonScanner {
+
+        private Scanner scanner;
+
+        public PersonScannerAdapter(Scanner scanner) {
+            this.scanner = scanner;
+        }
+
+        @Override
+        public Person read() throws IOException {
+            if (scanner.hasNext()) {
+                String[] inputArray = scanner.nextLine().split(" ");
+                Date birthDate = (new GregorianCalendar(Integer.parseInt(inputArray[5]), Integer.parseInt(inputArray[4]) - 1, Integer.parseInt(inputArray[3]))).getTime();
+                return new Person(inputArray[1], inputArray[2], inputArray[0], birthDate);
+            }
+            return null;
+        }
+
+        @Override
+        public void close() throws IOException {
+            this.scanner.close();
+        }
     }
 }
