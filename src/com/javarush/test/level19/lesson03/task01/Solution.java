6c12530d8c49f54709b8c2cb998dd6f02d4bd1d0
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-03 10:14:01 +0300

    Solved level19.lesson03.task01

--- src/com/javarush/test/level19/lesson03/task01/Solution.java
+++ src/com/javarush/test/level19/lesson03/task01/Solution.java
@@ -25,8 +25,18 @@ public class Solution {
         System.out.println(table.getHeaderText());
     }
 
-    public static class TableAdapter {
+    public static class TableAdapter implements BTable {
 
+        private ATable aTable;
+
+        public TableAdapter(ATable aTable) {
+            this.aTable = aTable;
+        }
+
+        @Override
+        public String getHeaderText() {
+            return "[" + this.aTable.getCurrentUserName() + "] : " + this.aTable.getTableName();
+        }
     }
 
     public interface ATable {
