60d650396c6a3a1d957b15590d431b53c1d809a2
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-03 11:11:26 +0300

    Added level19.lesson03.task04

---
+++ src/com/javarush/test/level19/lesson03/task04/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level19.lesson03.task04;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/* И еще один адаптер
+Адаптировать Scanner к PersonScanner.
+Классом-адаптером является PersonScannerAdapter.
+Данные в файле хранятся в следующем виде:
+Иванов Иван Иванович 31 12 1950
+
+В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен читать данные одного человека.
+*/
+
+public class Solution {
+    public static class PersonScannerAdapter {
+
+    }
+}
