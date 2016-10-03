ea293125e8c3c323cc42ba9305ddad15a401875b
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-03 10:20:21 +0300

    Added level19.lesson03.task03

---
+++ src/com/javarush/test/level19/lesson03/task03/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.test.level19.lesson03.task03;
+
+/* Адаптация нескольких интерфейсов
+Адаптировать IncomeData к Customer и Contact.
+Классом-адаптером является IncomeDataAdapter.
+Инициализируйте countries перед началом выполнения программы. Соответствие кода страны и названия:
+UA Ukraine
+RU Russia
+CA Canada
+Дополнить телефонный номер нулями до 10 цифр при необходимости (смотри примеры)
+Обратите внимание на формат вывода фамилии и имени человека
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    public static class IncomeDataAdapter {
+    }
+
+    public static interface IncomeData {
+        String getCountryCode();        //example UA
+
+        String getCompany();            //example JavaRush Ltd.
+
+        String getContactFirstName();   //example Ivan
+
+        String getContactLastName();    //example Ivanov
+
+        int getCountryPhoneCode();      //example 38
+
+        int getPhoneNumber();           //example 501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //example JavaRush Ltd.
+
+        String getCountryName();        //example Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //example Ivanov, Ivan
+
+        String getPhoneNumber();        //example +38(050)123-45-67
+    }
+}
