966765e73b36497797d210e43fbafd8cf039619c
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-03 12:14:45 +0300

    Solved level19.lesson05.task01

--- src/com/javarush/test/level19/lesson05/task01/Solution.java
+++ src/com/javarush/test/level19/lesson05/task01/Solution.java
@@ -7,7 +7,32 @@ package com.javarush.test.level19.lesson05.task01;
 Закрыть потоки ввода-вывода.
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = bufferedReader.readLine();
+        String fileName2 = bufferedReader.readLine();
+        bufferedReader.close();
+
+        int count = 1;
+
+        FileReader fileReader = new FileReader(fileName1);
+        FileWriter fileWriter = new FileWriter(fileName2);
+
+        while (fileReader.ready()) {
+            int data = fileReader.read();
+            if (count % 2 == 0) {
+                fileWriter.write(data);
+            }
+            count++;
+        }
+        fileReader.close();
+        fileWriter.close();
     }
 }
