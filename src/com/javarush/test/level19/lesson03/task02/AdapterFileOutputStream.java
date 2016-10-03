e7d5f9e24b92c3d2ca6bf7e3deb509fe11732224
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-03 10:14:51 +0300

    Added level19.lesson03.task02

---
+++ src/com/javarush/test/level19/lesson03/task02/AdapterFileOutputStream.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level19.lesson03.task02;
+
+/* Адаптер
+Используйте класс AdapterFileOutputStream, чтобы адаптировать FileOutputStream к новому интерфейсу AmigoStringWriter
+*/
+
+public class AdapterFileOutputStream {
+
+}
+
