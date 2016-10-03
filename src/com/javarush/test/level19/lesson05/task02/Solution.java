9e758dc2b29fe924818bdb54c5a400e4c2376abb
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-03 12:15:52 +0300

    Added level19.lesson05.task02

---
+++ src/com/javarush/test/level19/lesson05/task02/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level19.lesson05.task02;
+
+/* Считаем слово
+Считать с консоли имя файла.
+Файл содержит слова, разделенные знаками препинания.
+Вывести в консоль количество слов "world", которые встречаются в файле.
+Закрыть потоки. Не использовать try-with-resources
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
