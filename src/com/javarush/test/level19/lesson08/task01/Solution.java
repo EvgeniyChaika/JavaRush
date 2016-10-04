6c31ce708c06a79ca0ac2b77e970fff3048d5c11
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-04 23:14:31 +0300

    Added level19.lesson08.task01

---
+++ src/com/javarush/test/level19/lesson08/task01/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level19.lesson08.task01;
+
+/* Ридер обертка
+В методе main подмените объект System.out написанной вами ридер-оберткой по аналогии с лекцией
+Ваша ридер-обертка должна преобразовывать весь текст в заглавные буквы
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток.
+Вывести модифицированную строку в консоль.
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
