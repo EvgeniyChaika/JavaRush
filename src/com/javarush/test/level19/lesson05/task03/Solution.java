f749a3efd8b0891e626d8bc1957139bfc94d3e7d
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-04 22:58:33 +0300

    Added level19.lesson05.task03

---
+++ src/com/javarush/test/level19/lesson05/task03/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level19.lesson05.task03;
+
+/* Выделяем числа
+Считать с консоли 2 имени файла.
+Вывести во второй файл все числа, которые есть в первом файле.
+Числа выводить через пробел.
+Закрыть потоки. Не использовать try-with-resources
+
+Пример тела файла:
+12 text var2 14 8v 1
+
+Результат:
+12 14 1
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
