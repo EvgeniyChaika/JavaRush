6ce52f72e4f9653d277ed69eb98cad190d1158d9
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-04 23:11:16 +0300

    Solved level19.lesson05.task05

--- src/com/javarush/test/level19/lesson05/task05/Solution.java
+++ src/com/javarush/test/level19/lesson05/task05/Solution.java
@@ -8,7 +8,30 @@ http://ru.wikipedia.org/wiki/%D0%9F%D1%83%D0%BD%D0%BA%D1%82%D1%83%D0%B0%D1%86%D0
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = bufferedReader.readLine();
+        String fileName2 = bufferedReader.readLine();
+        bufferedReader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName1));
+        ArrayList<String> list = new ArrayList<>();
+        String nextLine;
+        while ((nextLine = fileReader.readLine()) != null) {
+            list.add(nextLine);
+        }
+        fileReader.close();
+
+        PrintWriter printWriter = new PrintWriter(new FileWriter(fileName2));
+        for (String aString : list) {
+            aString = aString.replaceAll("\\p{Punct}", "");
+            printWriter.println(aString);
+        }
+        printWriter.close();
     }
 }
