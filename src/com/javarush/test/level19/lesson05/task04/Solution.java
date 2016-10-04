dbf9936df16bc549510f734a7ec8fd01f6eb0683
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-04 23:05:55 +0300

    Added level19.lesson05.task04

---
+++ src/com/javarush/test/level19/lesson05/task04/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level19.lesson05.task04;
+
+/* Замена знаков
+Считать с консоли 2 имени файла.
+Первый Файл содержит текст.
+Заменить все точки "." на знак "!", вывести во второй файл.
+Закрыть потоки. Не использовать try-with-resources
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
