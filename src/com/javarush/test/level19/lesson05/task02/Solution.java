2dc5a47fb84fb3d5f698fff3b48560488ea9e36b
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-04 22:57:34 +0300

    Solved level19.lesson05.task02

--- src/com/javarush/test/level19/lesson05/task02/Solution.java
+++ src/com/javarush/test/level19/lesson05/task02/Solution.java
@@ -7,7 +7,35 @@ package com.javarush.test.level19.lesson05.task02;
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = bufferedReader.readLine();
+        bufferedReader.close();
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+        ArrayList<String> list = new ArrayList<>();
+        String nextLine;
+        while ((nextLine = fileReader.readLine()) != null) {
+            list.add(nextLine);
+        }
+        fileReader.close();
+        String[] wordArray;
+        int count = 0;
+        for (String aString : list) {
+            wordArray = aString.split(" ");
+            for (int i = 0; i < wordArray.length; i++) {
+                wordArray[i] = wordArray[i].replaceAll("[^A-Za-z0-9]", "");
+                if (wordArray[i].equals("world"))
+                    count++;
+            }
+        }
+        System.out.println(count);
     }
 }
