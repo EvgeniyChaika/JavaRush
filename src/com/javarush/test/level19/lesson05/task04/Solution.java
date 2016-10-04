7d67be864bf9516f87f967f4dcfd2d2c6d6ecd47
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-04 23:07:58 +0300

    Solved level19.lesson05.task04

--- src/com/javarush/test/level19/lesson05/task04/Solution.java
+++ src/com/javarush/test/level19/lesson05/task04/Solution.java
@@ -7,8 +7,30 @@ package com.javarush.test.level19.lesson05.task04;
 Закрыть потоки. Не использовать try-with-resources
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.ArrayList;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = bufferedReader.readLine();
+        String fileName2 = bufferedReader.readLine();
+        bufferedReader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName1));
+        ArrayList<String> list = new ArrayList<>();
+        String nextLine;
+        while ((nextLine = fileReader.readLine()) != null) {
+            list.add(nextLine);
+        }
+        fileReader.close();
+
+        PrintWriter printWriter = new PrintWriter(new FileWriter(fileName2));
+        for (String aString : list) {
+            aString = aString.replaceAll("\\.", "\\!");
+            printWriter.println(aString);
+        }
+        printWriter.close();
     }
 }
