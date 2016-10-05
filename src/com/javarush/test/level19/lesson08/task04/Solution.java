5493f98f0898afa5262bbd742d93333214d5bb89
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-05 21:13:23 +0300

    Added level19.lesson08.task04

---
+++ src/com/javarush/test/level19/lesson08/task04/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.test.level19.lesson08.task04;
+
+/* Решаем пример
+В методе main подмените объект System.out написанной вами ридер-оберткой по аналогии с лекцией
+Ваша ридер-обертка должна выводить на консоль решенный пример
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+
+Возможные операции: + - *
+Шаблон входных данных и вывода: a [знак] b = c
+Отрицательных и дробных чисел, унарных операторов - нет.
+
+Пример вывода:
+3 + 6 = 9
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("3 + 6 = ");
+        }
+    }
+}
+
