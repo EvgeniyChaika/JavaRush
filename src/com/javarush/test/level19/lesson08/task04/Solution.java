1e1c4726acb04f10cfc49054f2ee89c7fe688257
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-05 21:14:31 +0300

    Solved level19.lesson08.task04

--- src/com/javarush/test/level19/lesson08/task04/Solution.java
+++ src/com/javarush/test/level19/lesson08/task04/Solution.java
@@ -14,10 +14,33 @@ package com.javarush.test.level19.lesson08.task04;
 3 + 6 = 9
 */
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        testString.printSomething();
+        System.setOut(defaultPrintStream);
+
+        String result = byteArrayOutputStream.toString();
+        String[] resultArray = result.split(" ");
+        int firstNum = Integer.parseInt(resultArray[0]);
+        int secondNum = Integer.parseInt(resultArray[2]);
+        int arithmeticResult;
+
+        if (resultArray[1].equals("+"))
+            arithmeticResult = firstNum + secondNum;
+        else if (resultArray[1].equals("-"))
+            arithmeticResult = firstNum - secondNum;
+        else
+            arithmeticResult = firstNum * secondNum;
+
+        System.out.println(firstNum + " " + resultArray[1] + " " + secondNum + " = " + arithmeticResult);
     }
 
     public static class TestString {
