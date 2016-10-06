64e2c1afb89a6762bcce83adbce8248d6beea322
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-06 18:38:13 +0300

    Added level19.lesson10.home09

---
+++ src/com/javarush/test/level19/lesson10/home09/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level19.lesson10.home09;
+
+/* Контекстная реклама
+В методе main подмените объект System.out написанной вами реадер-оберткой
+Ваша реадер-обертка должна выводить на консоль контекстную рекламу после каждого второго println-а
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+
+Рекламный текст: "JavaRush - курсы Java онлайн"
+
+Пример вывода:
+first
+second
+JavaRush - курсы Java онлайн
+third
+fourth
+JavaRush - курсы Java онлайн
+fifth
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
