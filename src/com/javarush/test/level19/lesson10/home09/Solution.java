111a916b67f6aea5c7c751376b3f692c5f4325b1
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-06 18:40:23 +0300

    Solved level19.lesson10.home09

--- src/com/javarush/test/level19/lesson10/home09/Solution.java
+++ src/com/javarush/test/level19/lesson10/home09/Solution.java
@@ -18,10 +18,28 @@ JavaRush - курсы Java онлайн
 fifth
 */
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
 public class Solution {
     public static TestString testString = new TestString();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        testString.printSomething();
+        System.setOut(defaultPrintStream);
+
+        String[] byteArray = byteArrayOutputStream.toString().split("\\n");
+        for (int i = 0; i < byteArray.length; i++) {
+            System.out.println(byteArray[i]);
+            if (i % 2 != 0) {
+                System.out.println("JavaRush - курсы Java онлайн");
+            }
+        }
     }
 
     public static class TestString {
