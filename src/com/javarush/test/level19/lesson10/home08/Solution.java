2f0390f7a68d5f4b8b325e1d1c93a5c5b4c89b5c
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-06 18:37:30 +0300

    Solved level19.lesson10.home08

--- src/com/javarush/test/level19/lesson10/home08/Solution.java
+++ src/com/javarush/test/level19/lesson10/home08/Solution.java
@@ -16,7 +16,34 @@ package com.javarush.test.level19.lesson10.home08;
 огимА
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = bufferedReader.readLine();
+        bufferedReader.close();
+
+        ArrayList<String> fileList = new ArrayList<>();
+        String input;
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+        while ((input = fileReader.readLine()) != null) {
+            fileList.add(input);
+        }
+        fileReader.close();
+
+        ArrayList<String> resultWords = new ArrayList<>();
+        for (String current : fileList) {
+            resultWords.add(new StringBuilder().append(current).reverse().toString());
+        }
+
+        for (String current : resultWords)
+            System.out.println(current);
     }
 }
