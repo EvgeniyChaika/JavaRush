19fe64890e48a44b71afdb96d2570a1338ff0d40
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-06 18:55:18 +0300

    Added level19.lesson10.bonus02

---
+++ src/com/javarush/test/level19/lesson10/bonus02/FileConsoleWriter.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level19.lesson10.bonus02;
+
+/* Свой FileWriter
+Реализовать логику FileConsoleWriter
+Должен наследоваться от FileWriter
+При записи данных в файл, должен дублировать эти данные на консоль
+*/
+
+public class FileConsoleWriter {
+
+}
