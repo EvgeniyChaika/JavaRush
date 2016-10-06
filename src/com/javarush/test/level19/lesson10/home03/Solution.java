dbf8c0012455295922b51cca400e5363d2961c86
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-06 17:54:09 +0300

    Added level19.lesson10.home03

---
+++ src/com/javarush/test/level19/lesson10/home03/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level19.lesson10.home03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* Хуан Хуанович
+В метод main первым параметром приходит имя файла.
+В этом файле каждая строка имеет следующий вид:
+имя день месяц год
+где [имя] - может состоять из нескольких слов, разделенных пробелами, и имеет тип String
+[день] - int, [месяц] - int, [год] - int
+данные разделены пробелами
+
+Заполнить список PEOPLE импользуя данные из файла
+Закрыть потоки. Не использовать try-with-resources
+
+Пример входного файла:
+Иванов Иван Иванович 31 12 1987
+Вася 15 5 2013
+*/
+
+public class Solution {
+    public static final List<Person> PEOPLE = new ArrayList<Person>();
+
+    public static void main(String[] args) {
+    }
+
+}
