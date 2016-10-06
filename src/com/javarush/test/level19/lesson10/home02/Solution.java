b7dbe846e1b807a9d5ea512263f418177b032b8a
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-06 17:53:29 +0300

    Solved level19.lesson10.home02

--- src/com/javarush/test/level19/lesson10/home02/Solution.java
+++ src/com/javarush/test/level19/lesson10/home02/Solution.java
@@ -20,7 +20,64 @@ package com.javarush.test.level19.lesson10.home02;
 Петров
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        String fileName = args[0];
+        ArrayList<String> fileList = new ArrayList<>();
+        String input;
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+        while ((input = fileReader.readLine()) != null) {
+            fileList.add(input);
+        }
+        fileReader.close();
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<Double> values = new ArrayList<>();
+        String[] stringArray;
+        for (String aFileList : fileList) {
+            stringArray = aFileList.split(" ");
+            names.add(stringArray[0]);
+            values.add(Double.parseDouble(stringArray[1]));
+        }
+
+        Map<String, Double> map = new HashMap<>();
+        for (int i = 0; i < names.size(); i++) {
+            map.put(names.get(i), values.get(i));
+        }
+        ArrayList<String> ignoredNames = new ArrayList<>();
+        for (int i = 0; i < names.size() - 1; i++) {
+            for (int j = i + 1; j < names.size(); j++) {
+                if (names.get(i).equals(names.get(j)) && !ignoredNames.contains(names.get(i))) {
+                    double tmp = values.get(i) + values.get(j);
+                    map.put(names.get(i), tmp);
+                    ignoredNames.add(names.get(i));
+                }
+            }
+        }
+
+        double max = map.get(names.get(0));
+        for (Map.Entry<String, Double> entry : map.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+            }
+        }
+
+        ArrayList<String> theRichest = new ArrayList<>();
+        for (Map.Entry<String, Double> entry : map.entrySet()) {
+            if (entry.getValue() == max) {
+                theRichest.add(entry.getKey());
+            }
+        }
+
+        for (String aString : theRichest) {
+            System.out.print(aString + " ");
+        }
     }
 }
