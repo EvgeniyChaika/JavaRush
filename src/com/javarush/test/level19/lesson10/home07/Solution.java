e8181063ac40eff7bfdd0723102b2761a3778b88
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-06 18:34:35 +0300

    Solved level19.lesson10.home07

--- src/com/javarush/test/level19/lesson10/home07/Solution.java
+++ src/com/javarush/test/level19/lesson10/home07/Solution.java
@@ -10,8 +10,48 @@ package com.javarush.test.level19.lesson10.home07;
 длинное,короткое,аббревиатура
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.ArrayList;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            throw new IllegalArgumentException();
+        }
+
+        String fileName1 = args[0];
+        String fileName2 = args[1];
+        String input;
+        ArrayList<String> fileList = new ArrayList<>();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName1));
+        while ((input = fileReader.readLine()) != null) {
+            fileList.add(input);
+        }
+        fileReader.close();
+
+        ArrayList<String> resultWords = new ArrayList<>();
+        for (String aFileList : fileList) {
+            String[] stringArray = aFileList.split(" ");
+            for (String aStringArray : stringArray) {
+                if (aStringArray.length() > 6) {
+                    resultWords.add(aStringArray);
+                }
+            }
+        }
+
+        String result = "";
+        for (int i = 0; i < resultWords.size(); i++) {
+            if (i == resultWords.size() - 1) {
+                result = result + resultWords.get(i);
+            } else {
+                result = result + resultWords.get(i) + ",";
+            }
+        }
+
+        PrintWriter printWriter = new PrintWriter(new FileWriter(fileName2));
+        printWriter.println(result);
+        printWriter.close();
     }
 }
