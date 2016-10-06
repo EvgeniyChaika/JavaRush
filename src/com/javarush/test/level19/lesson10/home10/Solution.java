05c49762b9030d8cca9cc010e8da5189204e2641
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-06 18:44:22 +0300

    Solved level19.lesson10.home10

--- src/com/javarush/test/level19/lesson10/home10/Solution.java
+++ src/com/javarush/test/level19/lesson10/home10/Solution.java
@@ -42,7 +42,7 @@ public class Solution {
                 }
             }
 
-            if (result instanceof A) {
+            if (result instanceof C) {
                 C p = (C) result;
                 System.out.println(p.getClass().getSimpleName());
             }
