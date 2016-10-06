0b244faffce8a57b0e4d5d87eb1fb90edc32668d
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-06 18:18:44 +0300

    Solved level19.lesson10.home05

--- src/com/javarush/test/level19/lesson10/home05/Solution.java
+++ src/com/javarush/test/level19/lesson10/home05/Solution.java
@@ -7,7 +7,33 @@ package com.javarush.test.level19.lesson10.home05;
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        String fileName1 = args[0];
+        String input;
+        ArrayList<String> fileList = new ArrayList<>();
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName1));
+        while ((input = fileReader.readLine()) != null) {
+            fileList.add(input);
+        }
+        fileReader.close();
+
+        String fileName2 = args[1];
+        PrintWriter printWriter = new PrintWriter(new FileWriter(fileName2, true));
+        for (String aFileList : fileList) {
+            String[] stringArray = aFileList.split(" ");
+            String fileString = "";
+            for (String aStringArray : stringArray) {
+                if (aStringArray.matches(".*\\d.*")) {
+                    fileString = fileString + aStringArray + " ";
+                }
+            }
+            printWriter.println(fileString);
+        }
+        printWriter.close();
     }
 }
