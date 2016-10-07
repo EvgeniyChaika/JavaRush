f3041c2e39ea1394106d2d390c2fab340013be4f
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-07 10:44:00 +0300

    Solved level20.lesson02.task05

--- src/com/javarush/test/level20/lesson02/task05/Solution.java
+++ src/com/javarush/test/level20/lesson02/task05/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
         //you can find your_file_name.tmp in your TMP directory or fix outputStream/inputStream according to your real file location
         //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
         try {
-            File your_file_name = File.createTempFile("your_file_name", null);
+            File your_file_name = new File("/home/evgeniy/Documents/file.txt");
             OutputStream outputStream = new FileOutputStream(your_file_name);
             InputStream inputStream = new FileInputStream(your_file_name);
 
@@ -27,10 +27,13 @@ public class Solution {
             loadedObject.string1 = new String(); //string #3
             loadedObject.string2 = new String(); //string #4
 
-
             loadedObject.load(inputStream);
             //check here that the object variable equals to loadedObject - проверьте тут, что object и loadedObject равны
+            object.string1.print();
+            loadedObject.string1.print();
 
+            object.string2.print();
+            object.string2.print();
             outputStream.close();
             inputStream.close();
 
@@ -50,10 +53,39 @@ public class Solution {
 
         public void save(OutputStream outputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            if (string1 != null) {
+                printWriter.println(string1.number);
+            } else {
+                printWriter.println("null");
+            }
+            if (string2 != null) {
+                printWriter.println(string2.number);
+            } else {
+                printWriter.println("null");
+            }
+            printWriter.close();
         }
 
         public void load(InputStream inputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            java.lang.String s;
+            int count = countStrings;
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            if (!(s = br.readLine()).equals("null")) {
+                countStrings = Integer.parseInt(s) - 1;
+                string1 = new String();
+            } else {
+                string1 = null;
+            }
+            if (!(s = br.readLine()).equals("null")) {
+                countStrings = Integer.parseInt(s) - 1;
+                string2 = new String();
+            } else {
+                string2 = null;
+            }
+            countStrings = count;
+            br.close();
         }
     }
 
