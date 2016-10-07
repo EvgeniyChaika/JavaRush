6c72fd8bd44fb273df1c4a4bc72fa4f5b5d558a8
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-07 10:23:34 +0300

    Added level20.lesson02.task03

---
+++ src/com/javarush/test/level20/lesson02/task03/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level20.lesson02.task03;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/* Знакомство с properties
+В методе fillInPropertiesMap считайте имя файла с консоли и заполните карту properties данными из файла.
+Про .properties почитать тут - http://ru.wikipedia.org/wiki/.properties
+Реализуйте логику записи в файл и чтения из файла для карты properties.
+*/
+public class Solution {
+    public static Map<String, String> properties = new HashMap<>();
+
+    public void fillInPropertiesMap() {
+        //implement this method - реализуйте этот метод
+    }
+
+    public void save(OutputStream outputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+    }
+
+    public void load(InputStream inputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+    }
+}
