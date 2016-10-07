b91771f72015532a9610be1e13a8edaf69825982
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-07 11:41:55 +0300

    Solved level20.lesson04.task04

--- src/com/javarush/test/level20/lesson04/task04/Solution.java
+++ src/com/javarush/test/level20/lesson04/task04/Solution.java
@@ -1,10 +1,12 @@
 package com.javarush.test.level20.lesson04.task04;
 
+import java.io.Serializable;
+
 /* Как сериализовать static?
 Сделайте так, чтобы сериализация класса ClassWithStatic была возможной
 */
-public class Solution {
-    public static class ClassWithStatic {
+public class Solution implements Serializable {
+    public static class ClassWithStatic implements Serializable {
         public static String staticString = "it's test static string";
         public int i;
         public int j;
