1af89dd1fb603abc5149acb78dcfb2bf20235866
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-07 10:27:26 +0300

    Solved level20.lesson02.task03

--- src/com/javarush/test/level20/lesson02/task03/Solution.java
+++ src/com/javarush/test/level20/lesson02/task03/Solution.java
@@ -1,9 +1,10 @@
 package com.javarush.test.level20.lesson02.task03;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 /* Знакомство с properties
 В методе fillInPropertiesMap считайте имя файла с консоли и заполните карту properties данными из файла.
@@ -12,16 +13,37 @@ import java.util.Map;
 */
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
+    public static Properties props = new Properties();
 
-    public void fillInPropertiesMap() {
-        //implement this method - реализуйте этот метод
+    public static void main(String[] args) throws Exception {
+        new Solution().fillInPropertiesMap();
+        System.out.println(properties);
+        new Solution().save(new FileOutputStream("/home/evgeniy/Documents/2.properties"));
+    }
+
+    public void fillInPropertiesMap() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        InputStream fileProperties = new FileInputStream(bufferedReader.readLine());
+        bufferedReader.close();
+        load(fileProperties);
     }
 
     public void save(OutputStream outputStream) throws Exception {
-        //implement this method - реализуйте этот метод
+        PrintWriter printWriter = new PrintWriter(outputStream);
+        if (props.size() > 0) {
+            props.putAll(properties);
+        }
+        props.store(outputStream, "");
+        printWriter.close();
     }
 
     public void load(InputStream inputStream) throws Exception {
-        //implement this method - реализуйте этот метод
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        props.load(inputStream);
+        Set<String> list = props.stringPropertyNames();
+        for (String current : list) {
+            properties.put(current, props.getProperty(current));
+        }
+        bufferedReader.close();
     }
 }
