ccf3f8754a4e1b42ce4cb79de794e4a01198581a
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-07 11:43:18 +0300

    Solved level20.lesson04.task05

--- src/com/javarush/test/level20/lesson04/task05/Solution.java
+++ src/com/javarush/test/level20/lesson04/task05/Solution.java
@@ -1,17 +1,19 @@
 package com.javarush.test.level20.lesson04.task05;
 
+import java.io.Serializable;
+
 /* Как сериализовать что-то свое?
 Сделайте так, чтобы сериализация класса Object была возможной
 */
-public class Solution {
-    public static class Object {
+public class Solution implements Serializable {
+    public static class Object implements Serializable {
         public String string1;
         public String string2;
     }
 
     public static int countStrings;
 
-    public static class String {
+    public static class String implements Serializable {
         private final int number;
 
         public String() {
