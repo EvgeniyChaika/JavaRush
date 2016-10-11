07abc91cae204f38926793979f25b0d2eaab7c75
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-11 22:33:02 +0300

    Solved level20.lesson07.task04

--- src/com/javarush/test/level20/lesson07/task04/Solution.java
+++ src/com/javarush/test/level20/lesson07/task04/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.test.level20.lesson07.task04;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -17,20 +18,22 @@ import java.util.Date;
 6) проверить, что savedObject.string равна loadedObject.string
 7) обработать исключения
 */
-public class Solution {
+public class Solution implements Serializable {
     public static void main(String[] args) {
         System.out.println(new Solution(4));
     }
 
-    private final String pattern = "dd MMMM yyyy, EEEE";
-    private Date currentDate;
-    private int temperature;
+    private transient final String pattern = "dd MMMM yyyy, EEEE";
+    private transient Date currentDate;
+    private transient int temperature;
     String string;
+
+    public Solution() {
+    }
 
     public Solution(int temperature) {
         this.currentDate = new Date();
         this.temperature = temperature;
-
         string = "Today is %s, and current temperature is %s C";
         SimpleDateFormat format = new SimpleDateFormat(pattern);
         this.string = String.format(string, format.format(currentDate), temperature);
