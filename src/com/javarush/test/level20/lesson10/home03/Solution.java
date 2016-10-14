a33662259803a5fc285b6fab9a5834f3c36226a4
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-14 18:59:57 +0300

    Added level20.lesson10.home03

---
+++ src/com/javarush/test/level20/lesson10/home03/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level20.lesson10.home03;
+
+import java.io.Serializable;
+
+/* Найти ошибки
+Почему-то при сериализации/десериализации объекта класса B возникают ошибки.
+Найдите проблему и исправьте ее.
+Класс A не должен реализовывать интерфейсы Serializable и Externalizable.
+Сигнатура класса В не содержит ошибку :)
+Метод main не участвует в тестировании.
+*/
+public class Solution {
+    public static class A {
+        protected String name = "A";
+
+        public A(String name) {
+            this.name += name;
+        }
+    }
+
+    public class B extends A implements Serializable {
+        public B(String name) {
+            super(name);
+            this.name += name;
+        }
+    }
+}
