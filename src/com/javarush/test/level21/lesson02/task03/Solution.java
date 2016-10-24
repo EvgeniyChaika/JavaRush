5af89f973ebccc15efcf6eff031cefb9e0ed4801
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-24 20:49:00 +0300

    Solved level21.lesson02.task03

--- src/com/javarush/test/level21/lesson02/task03/Solution.java
+++ src/com/javarush/test/level21/lesson02/task03/Solution.java
@@ -5,7 +5,7 @@ package com.javarush.test.level21.lesson02.task03;
 */
 public class Solution {
     public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
-        return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
+        return c;
     }
 
 }
