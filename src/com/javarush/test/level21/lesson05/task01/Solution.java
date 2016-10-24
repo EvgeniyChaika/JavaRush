8ec8bcf2925ef60e0e3710370bf68746afab38ad
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2016-10-24 20:54:02 +0300

    Solved level21.lesson05.task01

--- src/com/javarush/test/level21/lesson05/task01/Solution.java
+++ src/com/javarush/test/level21/lesson05/task01/Solution.java
@@ -15,12 +15,32 @@ public class Solution {
         this.last = last;
     }
 
-    public boolean equals(Solution n) {
-        return n.first.equals(first) && n.last.equals(last);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Solution solution = (Solution) o;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) {
+            return false;
+        }
+        if (last != null ? !last.equals(solution.last) : solution.last != null) {
+            return false;
+        }
+
+        return true;
     }
 
+    @Override
     public int hashCode() {
-        return 31 * first.hashCode() + last.hashCode();
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
     }
 
     public static void main(String[] args) {
